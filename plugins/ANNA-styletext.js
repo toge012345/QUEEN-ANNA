296d89da448494698c4185a8fd9de7b7b88151ac
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-12 05:19:55 +0100

    ANNA-MD

--- plugins/ANNA-styletext.js
+++ plugins/ANNA-styletext.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text }) => {
 
   // If no key and text provided, show all styles of a default text
   if (words.length === 0 || !key || !textToStyle) {
-    let defaultText = 'ABHISHEK-SER';
+    let defaultText = 'QUEEN ANNA';
     let styledTexts = await Promise.all([...Array(34).keys()].map(i => stylizeText(defaultText, i + 1)));
     conn.reply(m.chat, styledTexts.join`\n\n`, m);
     return;
@@ -35,7 +35,7 @@ handler.exp = 0;
 export default handler;
 
 async function stylizeText(text, key) {
-  let res = await fetch(`https://inrl-web.onrender.com/api/fancy?text=${encodeURIComponent(text)}&key=${key}`);
+  let res = await fetch(`https://inrl-web-fkns.onrender.com/api/fancy?text=${encodeURIComponent(text)}&key=${key}`);
   let data = await res.json();
 
   // Use 'result' field for the styled text.
