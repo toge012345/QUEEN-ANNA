68c4fe18f4ab28eab4b4c29ea3f8100ee6756df0
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-16 01:46:55 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3", "Xlicon4"])}.mp3`
+let av = `./Assets/voice/${pickRandom(["anna", "hi", "menu", "test"])}.mp3`
 
-m.reply( `Hello ${taguser} Am Xlicon-V2 creatd by team Xlicon Need help?  type /help `)
+m.reply( `Hello ${taguser} Am 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰 creatd by 𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 Need help?  type /help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(Xlicon|Team Xlicon)$/i
+handler.customPrefix = /^(ANNA|TOGE INUMAKI)$/i
 handler.command = new RegExp
 
 export default handler
