3781a4611835454923c1b692636a4410d90c2330
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-16 02:09:34 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ global.dygp = 'https://chat.whatsapp.com/GpsTju4MUAbJmmZhHU8zAR'
 global.fgsc = 'https://github.com/toge012345/QUEEN-ANNA' 
 global.fgyt = 'https://youtube.com/@kenzo3146'
 global.fgpyp = 'https://youtube.com/@kenzo3146'
-global.fglog = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png' 
+global.fglog = 'ANNA.jpg' 
 
 //imagen
 global.imagen1 = fs.readFileSync('./Assets/ANNA.jpg')
