b903e741b952db41f86897cd499fa532ab7e7973
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-19 05:22:14 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/owner-report.js
+++ plugins/owner-report.js
@@ -3,8 +3,8 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `*⚠️Must Need 10 Characters To Send Report!*`;
   if (text.length > 1000) throw `*❌Too Long Only 1000 Characters Allowed!*`;
   const teks = `*╭───❮ REPORT ❯*\n*│*\n*│ Number:* wa.me/${m.sender.split`@`[0]}\n*│*\n*│*\n*│ Message:* ${text}\n*╰────────────⦁*`;
-  conn.reply('919074692450@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('919074692450@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('24105114159@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('24105114159@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`*✅Report Sent To The Developer Of This Bot. If This Is A Joke Or False It Will Be Ignored*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
