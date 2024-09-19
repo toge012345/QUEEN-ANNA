3f74786c74e02f767afbd1ed1173b26de43a3f86
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-19 20:20:06 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./src/mp3/${pickRandom(["Abhi", "Abhi", "Abhi", "Abhi", "Abhi"])}.mp3`
+let av = `./Assets/voice/${pickRandom(["menu", "test", "hi", "anna"])}.mp3`
 
 m.reply( `👋Hello ${taguser} Need Help?  Type .menu `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(.list|@919074692450)$/i
+handler.customPrefix = /^(.list|@24105114159)$/i
 handler.command = new RegExp
 
 export default handler
