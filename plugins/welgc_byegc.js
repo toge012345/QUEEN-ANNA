a735826fc785b3b0e24258869e25d3cb08ead63b
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-21 18:00:18 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/welgc_byegc.js
+++ plugins/welgc_byegc.js
@@ -3,11 +3,11 @@ let stikerwelgc = "./src/welgc.webp"
 let stikerbyegc = "./src/byegc.webp"
 if (command == 'welcomegc') {
 conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: ''𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯', body: '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️', sourceUrl: `https://github.com/AbhishekSuresh2/ABHISHEK-SER`, thumbnail: imagen1}}})  
+contextInfo: { externalAdReply: { title: ''𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸', body: '𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰', sourceUrl: `https://github.com/toge012345/QUEEN-ANNA`, thumbnail: imagen1}}})  
 }
 if (command == 'byegc') {
 conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: ''𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯', body: '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️', sourceUrl: `https://github.com/AbhishekSuresh2/ABHISHEK-SER`, thumbnail: imagen1}}})
+contextInfo: { externalAdReply: { title: ''𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸', body: '𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰', sourceUrl: `https://github.com/toge012345/QUEEN-ANNA`, thumbnail: imagen1}}})
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
