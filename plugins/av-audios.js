847f45080b1109219c04a108d6f10b2b4bf596c4
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-21 19:30:00 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -2,13 +2,13 @@ let handler = m => m
 handler.all = async function (m) {
     
   if (/^.menu$/i.test(m.text) ) {
-    let av = './src/mp3/menu.mp3'
+    let av = './Assets/voice/menu.mp3'
     this.sendPresenceUpdate('recording', m.chat);
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
   if (/^bgm$/i.test(m.text) ) {
-    let av = './src/mp3/bgm.mp3'
+    let av = './Assets/voice/anna.mp3'
     this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
   
