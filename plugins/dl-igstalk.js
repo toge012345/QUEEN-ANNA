6cfc72094d9e3612c58aadc0223ecf3af5f7dc90
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-09-26 19:33:24 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -1,41 +1,26 @@
-import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!args[0]) {
-    throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} asli_guru69`;
-  }
-  
-  try {
-    let response = await fetch(`https://www.guruapi.tech/api/igstalk?username=${args[0]}`);
-    
-    if (!response.ok) {
-      throw new Error('Failed to fetch data from API');
-    }
-
-    let res = await response.json();
-
+import fg from 'api-dylux'
+let handler= async (m, { conn, args, text, usedPrefix, command }) => {
+	
+    if (!args[0]) throw `✳️ Enter the Instagram Username\n\n📌Example: ${usedPrefix + command} ahmmikun` 
+    let res = await fg.igStalk(args[0])
     let te = `
 ┌──「 *STALKING* 
-▢ *🔖Name:* ${res.name} 
+▢ *🔖Number:* ${res.name} 
 ▢ *🔖Username:* ${res.username}
-▢ *👥Followers:* ${res.followers}
-▢ *🫂Following:* ${res.following}
-▢ *📌Bio:* ${res.biography}
-▢ *🏝️Posts:* ${res.posts}
-▢ *🔗 Link:* https://instagram.com/${res.username.replace(/^@/, '')}
-└────────────`;
+▢ *👥followers:* ${res.followersH}
+▢ *🫂following:* ${res.followingH}
+▢ *📌Bio:* ${res.description}
+▢ *🏝️Posts:* ${res.postsH}
 
-    await conn.sendFile(m.chat, res.profile_picture, 'profile_picture.png', te, m);
+▢ *🔗 Link* : https://instagram.com/${res.username.replace(/^@/, '')}
+└────────────`
 
-  } catch (error) {
-    console.error(error);
-    throw 'An error occurred while fetching the Instagram profile. Please try again later.';
-  }
-};
+     await conn.sendFile(m.chat, res.profilePic, 'tt.png', te, m)
+     
+}
+handler.help = ['igstalk']
+handler.tags = ['downloader']
+handler.command = ['igstalk'] 
 
-handler.help = ['igstalk'];
-handler.tags = ['downloader'];
-handler.command = ['igstalk'];
-
-export default handler;
-
+export default handler
