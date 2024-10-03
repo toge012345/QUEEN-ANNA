d4744e6e5b01777744444759dac2ff3b4c3d02b7
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-10-03 20:34:01 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/main-ABHI.js
+++ plugins/main-ABHI.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 
 ≡  *𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸*   GROUPS
 ▢ Join Public Bot Group And Support
-https://chat.whatsapp.com/GpsTju4MUAbJmmZhHU8zAR
+https://chat.whatsapp.com/BASoDcE8VL01jjVaraH1Ol
 ─────────────
 ▢ *Owner Telegram*
 https://t.me/lawliet_kfx
