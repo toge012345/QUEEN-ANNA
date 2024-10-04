99bfd14f1cd6cc3515ddb70a6d0c4781446b1c64
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-10-04 17:46:52 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/fun-question.js
+++ plugins/fun-question.js
@@ -1,30 +1,30 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `🎯Give Me A Question *📌Example:*\n\n*${usedPrefix + command}* Am I Smart?`;
+  if (!text) throw `✳️ *Example:*\n\n*${usedPrefix + command}* Am I ugly?`
 
-  m.react('❓');
-  conn.sendPresenceUpdate('composing', m.chat);
+  m.react('🫣')
+  conn.sendPresenceUpdate('composing', m.chat)
 
-  let res = await fetch(`https://gurugpt.cyclic.app/gpt4?prompt=${encodeURIComponent(text)}&model=llama`);
-  let json = await res.json();
+  let res = await fetch(
+    `https://gurugpt.cyclic.app/gpt4?prompt=${encodeURIComponent(text)}&model=llama`
+  )
+  let json = await res.json()
 
   if (json && json.data) {
-    const answer = json.data;
+    const answer = json.data
 
-    m.reply(`╭────❮  *🎯ANSWER* ❯
-│     
-│  *Question:* ${text}
-│  *Answer:* ${answer}
-│ 
-╰────────────⦁`);
+    m.reply(`≡ *RESPONSE*
+    
+▢ *Question:* ${text}
+▢ *Answer:* ${answer}`)
   } else {
-    throw '🎯No Valid Response Received From The API.';
+    throw 'No valid response received from the API.'
   }
-};
+}
 
-handler.help = ['question'];
-handler.tags = ['fun'];
-handler.command = ['question', 'q'];
+handler.help = ['question']
+handler.tags = ['fun']
+handler.command = ['question', 'q']
 
-export default handler;
+export default handler
