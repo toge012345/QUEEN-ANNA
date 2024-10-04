9bcb95d223f9ad1d6220f7ffbfa68ad0e8c1e350
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <167923490+toge012345@users.noreply.github.com>
2024-10-04 04:07:37 +0100

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     var vn = "./Assets/ALIVE.mp3"
     let url = "https://github.com/toge012345"
     let murl = "https://youtube.com/@kenzo3146?si=_v7ZRTjkqmLKt0HK"
-    let img = "https://raw.githubusercontent.com/toge012345/QUEEN-ANNA/main/Assets/ANNA.jpg"
+    let img = "https://i.ibb.co/Btk5dcq/ANNA.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '24105114159@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -24,7 +24,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
           title: "𝐐𝐔𝐄𝐄𝐍 𝐀𝐍𝐍𝐀 𝐈𝐒 𝐀𝐋𝐈𝐕𝐄 🌟",
           body: "𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙽𝙰",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/GpsTju4MUAbJmmZhHU8zAR',
+          sourceUrl: 'https://chat.whatsapp.com/BASoDcE8VL01jjVaraH1Ol',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
